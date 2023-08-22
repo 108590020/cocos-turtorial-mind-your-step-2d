ef9467ad009a2077372e37567653e6781e5e3053
Yu-Kai Lin <scott421722@gmail.com>
2023-08-22 12:10:16 +0800

    game finished except Levels

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,21 +1,59 @@
-import { _decorator, Component, Node, Prefab, CCInteger, instantiate } from 'cc';
+import { _decorator, Component, Node, Prefab, CCInteger, instantiate, Label, Vec3 } from 'cc';
+import { BLOCK_SIZE, PlayerController } from './PlayerController';
 const { ccclass, property } = _decorator;
+
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
 
 enum BlockType{
     BT_NONE,
     BT_STONE,
 };
 
-export const BLOCK_SIZE = 40;
-
 @ccclass('GameManager')
 export class GameManager extends Component {
 
+    @property({ type: Node })
+    public startMenu: Node | null = null; 
+    @property({ type: PlayerController }) 
+    public playerCtrl: PlayerController | null = null; 
+    @property({type: Label}) 
+    public stepsLabel: Label|null = null; 
     @property({type: Prefab})
     public boxPrefab: Prefab|null = null;
     @property({type: CCInteger})
     public roadLength: number = 50;
     private _road: BlockType[] = [];
+
+    setCurState (value: GameState) {
+        switch(value) {
+            case GameState.GS_INIT:
+                this.init();     
+                break;
+            case GameState.GS_PLAYING:
+                if (this.startMenu) {
+                    this.startMenu.active = false;
+                }
+              
+                if (this.stepsLabel) {
+                    this.stepsLabel.string = '0';   
+                }
+              
+                setTimeout(() => {      //直接设置active会直接开始监听鼠标事件，做了一下延迟处理
+                    if (this.playerCtrl) {
+                        this.playerCtrl.setInputActive(true);
+                    }
+                }, 0.1);
+            
+                break;
+            case GameState.GS_END:
+
+                break;
+        }
+    }
 
     spawnBlockByType(type: BlockType) {
         if (!this.boxPrefab) {
@@ -51,16 +89,56 @@ export class GameManager extends Component {
         for (let j = 0; j < this._road.length; j++) {
             let block: Node | null = this.spawnBlockByType(this._road[j]);
             if (block) {
+                //console.info("create block");
                 this.node.addChild(block);
+                console.info("create block");
                 block.setPosition(j * BLOCK_SIZE, 0, 0);
+                console.info(block.getPosition());
             }
         }
 
         //console.info(this._road);
     }
 
+    onStartButtonClicked() {    
+        this.setCurState(GameState.GS_PLAYING);
+    }
+
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength) {
+            if (this._road[moveIndex] == BlockType.BT_NONE) {   
+  
+                this.setCurState(GameState.GS_INIT)
+            }
+        } else {    // finish max length            
+            this.setCurState(GameState.GS_INIT);
+        }
+    }
+
+    onPlayerJumpEnd(moveIndex: number) {
+        if (this.stepsLabel) {
+            this.stepsLabel.string = '' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
+        }
+        this.checkResult(moveIndex);
+    }
+
     start() {
-        this.generateRoad()
+        this.setCurState(GameState.GS_INIT);
+        this.playerCtrl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+    }
+
+    init() {
+        if (this.startMenu) {
+          this.startMenu.active = true;
+      }
+
+      this.generateRoad();
+
+      if (this.playerCtrl) {
+          this.playerCtrl.setInputActive(false);
+          this.playerCtrl.node.setPosition(Vec3.ZERO);
+          this.playerCtrl.reset();
+      }
     }
 
     update(deltaTime: number) {
--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -12,29 +12,32 @@ export class PlayerController extends Component {
     /*physics formula => P_1 = P_0 + v*t 
     P_1 is the final position, P_0 is the original position, 
     v is the speed of the object, and t is the unit time*/
-
-    //used to judge if the player is jumping.
     private _startJump: boolean = false;
-    //the number of steps will the player jump, should be 1 or 2. determined by which mouse button is clicked.
     private _jumpStep: number = 0;
-    //the time it takes for the player to jump once.
     private _jumpTime: number = 0.1;
-    //the time that the player's current jump action has taken, should be set to 0 each time the player jumps, when it reaches the value of `_jumpTime`, the jump action is completed.
     private _curJumpTime: number = 0;
-    // The player's current vertical speed, used to calculate the Y value of position when jumping.
     private _curJumpSpeed: number = 0;
-    // The current position of the player, used as the original position in the physics formula.
     private _curPos: Vec3 = new Vec3();
-    //movement calculated by deltaTime.
     private _deltaPos: Vec3 = new Vec3(0, 0, 0);
-    // store the final position of the player, when the player's jumping action ends, it will be used directly to avoid cumulative errors.
+
     private _targetPos: Vec3 = new Vec3();
+
+    private _curMoveIndex: number = 0;
     
     start() {
-        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        //input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    setInputActive(active: boolean) {
+        if (active) {
+            input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
     }
 
     reset() {
+        this._curMoveIndex = 0;
     } 
 
     onMouseUp(event: EventMouse) {
@@ -47,25 +50,18 @@ export class PlayerController extends Component {
 
     jumpByStep(step: number) {
         if (this._startJump) {
-            //if the player is jumping, do nothing.
             return;
         }
-        //mark player is jumping.
         this._startJump = true;
-        //record the number of steps the jumping action will take.
         this._jumpStep = step;
-        //set to 0 when a new jumping action starts
         this._curJumpTime = 0;
 
         const clipName = step == 1 ? 'oneStep' : 'twoStep';
         const state = this.BodyAnim.getState(clipName);
         this._jumpTime = state.duration;
 
-        //because the player will finish the jumping action in the fixed duration(_jumpTime), so it needs to calculate jump speed here.
         this._curJumpSpeed = this._jumpStep * BLOCK_SIZE / this._jumpTime;
-        //copy the current position of the node which will be used when calculating the movement.
         this.node.getPosition(this._curPos);
-        //calculate the final position of the node which will be used when the jumping action ends.
         Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep * BLOCK_SIZE, 0, 0));
 
         if (this.BodyAnim) {
@@ -75,28 +71,25 @@ export class PlayerController extends Component {
                 this.BodyAnim.play('twoStep');
             }
         }
+
+        this._curMoveIndex += step;
+    }
+
+    onOnceJumpEnd() {
+        this.node.emit('JumpEnd', this._curMoveIndex);
     }
 
     update(deltaTime: number) {
-        //we only do something when the player is jumping.
         if (this._startJump) {
-            //accumulate the jumping time.
             this._curJumpTime += deltaTime;
-            //check if it reaches the jump time.
             if (this._curJumpTime > this._jumpTime) {
-                // When the jump ends, set the player's position to the target position. 
                 this.node.setPosition(this._targetPos);
-                //clear jump state
                 this._startJump = false;
+                this.onOnceJumpEnd();
             } else {
-                //if it still needs to move.
-                // copy the position of the node.
                 this.node.getPosition(this._curPos);
-                //calculate the offset x by using deltaTime and jumping speed.
                 this._deltaPos.x = this._curJumpSpeed * deltaTime;
-                //calculate the final pos by adding deltaPos to the original position
                 Vec3.add(this._curPos, this._curPos, this._deltaPos);
-                //update the position of the player.
                 this.node.setPosition(this._curPos);
             }
         }
